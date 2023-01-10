29a68f80ee8acfedd85c5ed12c3790b773e41015
Samuellekpor <samuellekpor@gmail.com>
2023-01-10 14:41:40 +0000

    Add private and public methods

--- person.rb
+++ person.rb
@@ -9,5 +9,14 @@ class Person
   attr_reader :id
   attr_accessor :name
   attr_accessor :age
+  
+  private
+  def is_of_age?
+    @age >= 18
+  end
 
+  public
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 end
