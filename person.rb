12b4ca5fa5298729b07591af886e1d839989d680
Samuellekpor <samuellekpor@gmail.com>
2023-01-12 23:02:59 +0000

    Create the many-to-many relationship between Person and Book

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './nameable'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rental
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
   end
 
   def correct_name
