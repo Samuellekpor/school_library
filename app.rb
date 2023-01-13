d98e300dcd4c95984c9f2bd88cb600fea2a2f42d
Samuellekpor <samuellekpor@gmail.com>
2023-01-13 17:18:58 +0000

    Add methods to list all people, books and rentals

--- app.rb
+++ app.rb
@@ -12,4 +12,26 @@ class App
     @people = []
     @rentals = []
   end
+
+  def list_all_books
+    @books&.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  def list_all_people
+    @people&.each do |person|
+      puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+    end
+  end
+
+  def list_all_rentals(id)
+    @people.each do |person|
+      next unless person.id == id
+
+      person.rentals.each do |rental|
+        puts "Book: #{rental.book.title}, Person: #{rental.person.name}, Date: #{rental.date}"
+      end
+    end
+  end
 end
